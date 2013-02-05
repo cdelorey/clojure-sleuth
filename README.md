946e70f8f4569412c0e6d672f740152b754b5fbd
cdelorey <cdelorey@fake.com>
2013-02-05 12:41:25 -0400

    Update readme and project.clj.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# sleuth
+# CLojure-Sleuth
 
 A Clojure clone of the ASCII murder mystery game Sleuth:
 http://en.wikipedia.org/wiki/Sleuth_(video_game)
@@ -11,4 +11,4 @@ http://en.wikipedia.org/wiki/Sleuth_(video_game)
 
 Copyright © 2013 cdelorey
 
-Distributed under the MIT/X11 License.
+Distributed under the Eclipse Public License.
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sleuth "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure clone of the murder myster game Sleuth"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clojure-lanterna "0.9.2"]])
