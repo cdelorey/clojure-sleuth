f23feaa0001e74803f7d0880c1cd810696b15d00
cdelorey <cdelorey@fake.com>
2014-05-31 19:36:13 -0300

    Fixed unit tests

--- spec/sleuth/guests_spec.clj
+++ spec/sleuth/guests_spec.clj
@@ -2,11 +2,11 @@
   (:require [speclj.core :refer :all]
             [sleuth.entities.guests :refer :all]))
 
-(describe "get-guest-names"
-          (with test-world (assoc-in {} [:entities :guests] (assoc {}
-                                                                  :sondra (->Guest "Sondra Levinson" " " 0 [0 0] " ")
-                                                                  :yolanda (->Guest "Yolanda Gould" " " 0 [0 0] " ")
-                                                                  :joey (->Guest "Joey Fitzgibbon" " " 0 [0 0] " "))))
-
-          (it "should return a sequence of the first names of all guests in the world."
-              (should= '("Joey" "Yolanda" "Sondra") (get-guest-names @test-world))))
+(before
+ (with test-world (assoc-in {} [:entities :guests] (assoc {}
+                                                                  :sondra (->Guest "Sondra Levinson" " " 0 [0 0] " " false)
+                                                                  :yolanda (->Guest "Yolanda Gould" " " 0 [0 0] " " false)
+                                                                  :joey (->Guest "Joey Fitzgibbon" " " 0 [0 0] " " false))))
+ (describe "get-guest-names"
+           (it "should return a sequence of the first names of all guests in the world."
+               (should= '("Joey" "Yolanda" "Sondra") (get-guest-names @test-world)))))
